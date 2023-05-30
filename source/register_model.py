970d155ea585572c80c8bd9702bac3ce025f2a92
user-vi <50022091+user-vi@users.noreply.github.com>
2023-05-30 22:24:11 +0300

    add mlflow

--- source/register_model.py
+++ source/register_model.py
@@ -70,10 +70,16 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse ASC"]
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    run_id = best_run.info.run_id
+    mlflow.register_model(model_uri=f"runs:/{run_id}/model", name=EXPERIMENT_NAME)
 
 
 if __name__ == '__main__':
